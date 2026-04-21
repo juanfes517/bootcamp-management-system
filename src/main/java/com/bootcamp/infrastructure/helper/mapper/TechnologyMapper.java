61433b58c86f648a87e24cc86bd640abcf9c64ef
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-21 14:20:41 -0500

    test: add unit tests for technology creation

--- src/main/java/com/bootcamp/infrastructure/helper/mapper/TechnologyMapper.java
+++ src/main/java/com/bootcamp/infrastructure/helper/mapper/TechnologyMapper.java
@@ -5,12 +5,17 @@ import com.bootcamp.infrastructure.input.rest.dto.request.TechnologyRequestDTO;
 import com.bootcamp.infrastructure.input.rest.dto.response.TechnologyResponseDTO;
 import com.bootcamp.infrastructure.output.r2dbc.entity.TechnologyEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TechnologyMapper {
 
     Technology toDomain(TechnologyEntity technologyEntity);
+
+    @Mapping(target = "id", ignore = true)
     Technology toDomain(TechnologyRequestDTO technologyRequestDTO);
+
     TechnologyEntity toEntity(Technology technology);
+
     TechnologyResponseDTO toResponseDTO(Technology technology);
 }
