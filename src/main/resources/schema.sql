dfb65dcac899c329ce237777c96cb232259dbf32
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-22 14:15:48 -0500

    chore: add SQL schemas for capability and technology_capability tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,16 @@ CREATE TABLE IF NOT EXISTS technology (
     name VARCHAR(50) UNIQUE NOT NULL,
     description VARCHAR(90) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS capability (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description VARCHAR(90) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS technology_capability (
+    id BIGSERIAL PRIMARY KEY,
+    technology_id BIGSERIAL NOT NULL REFERENCES technology(id),
+    capability_id BIGSERIAL NOT NULL REFERENCES capability(id)
+);
+
