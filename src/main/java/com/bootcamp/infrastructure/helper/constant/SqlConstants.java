c14695511d1004c8aadd5469562701ca7f08b30d
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-04-28 14:53:32 -0500

    feat: add custom queries to get capabilities with specific ordering

---
+++ src/main/java/com/bootcamp/infrastructure/helper/constant/SqlConstants.java
@@ -0,0 +1,58 @@
+package com.bootcamp.infrastructure.helper.constant;
+
+public class SqlConstants {
+
+    private SqlConstants() {
+    }
+
+    public static final String FIND_ALL_QUERY = """
+            SELECT c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                    t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT c.*
+                FROM capability c
+                LIMIT :size OFFSET :offset
+            ) c
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON t.id = tc.technology_id
+            """;
+
+    public static final String FIND_ALL_BY_NAME_QUERY = """
+            SELECT c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                    t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT c.*
+                FROM capability c
+                ORDER BY c.name %s
+                LIMIT :size OFFSET :offset
+            ) c
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON t.id = tc.technology_id
+            ORDER BY c.name %s, t.id
+            """;
+
+    public static final String FIND_ALL_BY_TECHNOLOGY_COUNT_QUERY = """
+            SELECT c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                    t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT c.*, COUNT(tc.technology_id) as technology_count
+                FROM capability c
+                LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+                GROUP BY c.id
+                ORDER BY technology_count %s
+                LIMIT :size OFFSET :offset
+            ) c
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON t.id = tc.technology_id
+            ORDER BY c.technology_count %s, c.id, t.id
+            """;
+
+    public static final String CAPABILITY_ID = "capability_id";
+    public static final String SIZE_STRING = "size";
+    public static final String OFFSET_STRING = "offset";
+    public static final String CAPABILITY_NAME = "capability_name";
+    public static final String CAPABILITY_DESCRIPTION = "capability_description";
+    public static final String TECHNOLOGY_ID = "technology_id";
+    public static final String TECHNOLOGY_NAME = "technology_name";
+    public static final String TECHNOLOGY_DESCRIPTION = "technology_description";
+}
