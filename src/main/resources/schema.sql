21edc6fcb49676cd2d3393267ab84ba0b4215f09
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-05-04 13:05:08 -0500

    chore: update database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,3 +16,16 @@ CREATE TABLE IF NOT EXISTS technology_capability (
     capability_id BIGSERIAL NOT NULL REFERENCES capability(id)
 );
 
+CREATE TABLE IF NOT EXISTS bootcamp (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description VARCHAR(90) NOT NULL,
+    release_date TIMESTAMP NOT NULL,
+    duration_days INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS capability_bootcamp (
+    id BIGSERIAL PRIMARY KEY,
+    capability_id BIGSERIAL NOT NULL REFERENCES capability(id),
+    bootcamp_id BIGSERIAL NOT NULL REFERENCES bootcamp(id)
+);
