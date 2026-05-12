b785c2927e815196aa872944d3f112c7a6a0edcc
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-05-11 22:48:39 -0500

    feat: add ports and use case for listing bootcamps

--- src/main/java/com/bootcamp/domain/api/IBootcampServicePort.java
+++ src/main/java/com/bootcamp/domain/api/IBootcampServicePort.java
@@ -1,9 +1,12 @@
 package com.bootcamp.domain.api;
 
 import com.bootcamp.domain.model.Bootcamp;
+import com.bootcamp.domain.model.PageRequest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface IBootcampServicePort {
 
     Mono<Bootcamp> registerBootcamp(Bootcamp bootcamp);
+    Flux<Bootcamp> getAllBootcamps(PageRequest pageRequest);
 }
