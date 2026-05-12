2ddec07b42b291612238f81c43e6e2e9f222af93
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-05-11 22:51:59 -0500

    feat: add endpoint to list bootcamps

--- src/main/java/com/bootcamp/infrastructure/input/rest/controller/BootcampController.java
+++ src/main/java/com/bootcamp/infrastructure/input/rest/controller/BootcampController.java
@@ -2,18 +2,20 @@ package com.bootcamp.infrastructure.input.rest.controller;
 
 import com.bootcamp.application.handler.IBootcampHandler;
 import com.bootcamp.domain.model.Bootcamp;
+import com.bootcamp.domain.model.PageRequest;
+import com.bootcamp.infrastructure.helper.constant.ApiConstants;
 import com.bootcamp.infrastructure.helper.mapper.BootcampMapper;
 import com.bootcamp.infrastructure.input.rest.dto.request.BootcampRequestDTO;
 import com.bootcamp.infrastructure.input.rest.dto.response.BootcampResponseDTO;
+import com.bootcamp.infrastructure.input.rest.enums.BootcampSortBy;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Validated
@@ -35,5 +37,25 @@ public class BootcampController {
                 .map(savedBootcamp -> ResponseEntity
                         .status(HttpStatus.CREATED)
                         .body(bootcampMapper.toResponse(savedBootcamp)));
+    }
+
+    @GetMapping
+    public Flux<BootcampResponseDTO> getAllBootcamps(
+            @RequestParam(defaultValue = ApiConstants.DEFAULT_PAGE) @Min(0) int page,
+            @RequestParam(defaultValue = ApiConstants.DEFAULT_SIZE) @Min(1) int size,
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortOrder
+    ) {
+        BootcampSortBy.fromString(sortBy);
+        PageRequest pageRequest = PageRequest.builder()
+                .page(page)
+                .size(size)
+                .sortBy(sortBy)
+                .sortOrder(PageRequest.SortOrder.fromString(sortOrder))
+                .build();
+
+        return bootcampHandler
+                .getAllBootcamps(pageRequest)
+                .map(bootcampMapper::toResponse);
     }
 }
