0909e9a6e273bf78bc0f6eef0870564895f8705e
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-05-11 22:50:07 -0500

    feat: add persistence port and custom queries to list bootcamps

--- src/main/java/com/bootcamp/infrastructure/helper/constant/SqlConstants.java
+++ src/main/java/com/bootcamp/infrastructure/helper/constant/SqlConstants.java
@@ -48,7 +48,7 @@ public class SqlConstants {
             """;
 
     public static final String FIND_ALL_CAPABILITIES_BY_IDS = """
-            SELECT c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,\s
+            SELECT c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
                     t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
             FROM capability c
             LEFT JOIN technology_capability tc ON c.id = tc.capability_id
@@ -56,12 +56,69 @@ public class SqlConstants {
             WHERE c.id IN (:ids)
             """;
 
-    public static final String CAPABILITY_ID = "capability_id";
+    public static final String FIND_ALL_BOOTCAMPS = """
+            SELECT b.id AS bootcamp_id, b.name AS bootcamp_name, b.description AS bootcamp_description, b.release_date AS bootcamp_release_date, b.duration_days AS bootcamp_duration_days,
+                c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT b.*
+                FROM bootcamp b
+                LIMIT :size OFFSET :offset
+            ) b
+            LEFT JOIN capability_bootcamp cb ON b.id = cb.bootcamp_id
+            LEFT JOIN capability c ON cb.capability_id = c.id
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON tc.technology_id = t.id
+            ORDER BY b.id, c.id, t.id
+            """;
+
+    public static final String FIND_ALL_BOOTCAMPS_ORDER_BY_NAME = """
+            SELECT b.id AS bootcamp_id, b.name AS bootcamp_name, b.description AS bootcamp_description, b.release_date AS bootcamp_release_date, b.duration_days AS bootcamp_duration_days,
+                c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT b.*
+                FROM bootcamp b
+                ORDER BY b.name %s
+                LIMIT :size OFFSET :offset
+            ) b
+            LEFT JOIN capability_bootcamp cb ON b.id = cb.bootcamp_id
+            LEFT JOIN capability c ON cb.capability_id = c.id
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON tc.technology_id = t.id
+            ORDER BY b.name %s, c.id, t.id
+            """;
+
+    public static final String FIND_ALL_BOOTCAMPS_ORDER_BY_CAPABILITY_COUNT = """
+            SELECT b.id AS bootcamp_id, b.name AS bootcamp_name, b.description AS bootcamp_description, b.release_date AS bootcamp_release_date, b.duration_days AS bootcamp_duration_days,
+                c.id AS capability_id, c.name AS capability_name, c.description AS capability_description,
+                t.id AS technology_id, t.name AS technology_name, t.description AS technology_description
+            FROM (
+                SELECT b.*, COUNT(cb.capability_id) AS capability_count
+                FROM bootcamp b
+                LEFT JOIN capability_bootcamp cb ON b.id = cb.bootcamp_id
+                GROUP BY b.id
+                ORDER BY capability_count %s
+                LIMIT :size OFFSET :offset
+            ) b
+            LEFT JOIN capability_bootcamp cb ON b.id = cb.bootcamp_id
+            LEFT JOIN capability c ON cb.capability_id = c.id
+            LEFT JOIN technology_capability tc ON c.id = tc.capability_id
+            LEFT JOIN technology t ON tc.technology_id = t.id
+            ORDER BY b.capability_count %s, b.id, c.id, t.id
+            """;
+
     public static final String SIZE_STRING = "size";
     public static final String OFFSET_STRING = "offset";
+    public static final String CAPABILITY_ID = "capability_id";
     public static final String CAPABILITY_NAME = "capability_name";
     public static final String CAPABILITY_DESCRIPTION = "capability_description";
     public static final String TECHNOLOGY_ID = "technology_id";
     public static final String TECHNOLOGY_NAME = "technology_name";
     public static final String TECHNOLOGY_DESCRIPTION = "technology_description";
+    public static final String BOOTCAMP_ID = "bootcamp_id";
+    public static final String BOOTCAMP_NAME = "bootcamp_name";
+    public static final String BOOTCAMP_DESCRIPTION = "bootcamp_description";
+    public static final String BOOTCAMP_RELEASE_DATE = "bootcamp_release_date";
+    public static final String BOOTCAMP_DURATION_DAYS = "bootcamp_duration_days";
 }
