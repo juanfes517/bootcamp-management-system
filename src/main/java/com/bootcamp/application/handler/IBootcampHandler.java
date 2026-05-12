27b5992996c7cf9dd067d92e2a6a1a5c55a2f7f0
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2026-05-11 22:51:38 -0500

    feat: add method in handler layer to list bootcamps

--- src/main/java/com/bootcamp/application/handler/IBootcampHandler.java
+++ src/main/java/com/bootcamp/application/handler/IBootcampHandler.java
@@ -1,6 +1,8 @@
 package com.bootcamp.application.handler;
 
 import com.bootcamp.domain.model.Bootcamp;
+import com.bootcamp.domain.model.PageRequest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface IBootcampHandler {
 
     Mono<Bootcamp> registerBootcamp(Bootcamp bootcamp, List<Long> capabilityIds);
+    Flux<Bootcamp> getAllBootcamps(PageRequest pageRequest);
 }
